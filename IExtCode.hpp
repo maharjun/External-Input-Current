40b839fa55bd4ab97fbe39d6b42fa647f05ab573
maharjun <arjun210493@gmail.com>
2015-09-28 20:43:57 +0530

    Added support to incorporate TimeDelNetSim as Sub-Library
    Added macros in IExtCode.cpp and IExtCode.hpp to alter
    includes in presence of the macro TIME_DEL_NET_SIM_AS_SUB
    
    Library Versions:
    
       MexMemoryInterfacing - db83594 Added Trim to assignmxArray + Renaming
       RandomNumGen         - 729fe91 Added Support for being run as a Sub-Library

--- IExtCode.hpp
+++ IExtCode.hpp
@@ -5,8 +5,18 @@
 #include <mex.h>
 #undef printf
 
-#include "..\..\..\MexMemoryInterfacing\Headers\MexMem.hpp"
-#include "..\..\..\RandomNumGen\Headers\FiltRandomTBB.hpp"
+#if defined TIME_DEL_NET_SIM_AS_SUB
+	#define HEADER_PATHS_TDNS ..
+#elif !defined HEADER_PATHS_TDNS
+	#define HEADER_PATHS_TDNS .
+#endif
+
+#define SETQUOTE(A) #A
+#define JOIN_STRING(A,B,C) SETQUOTE(A##B##C)
+#define JOIN_LIB_PATH(PRE, CENT, POST) JOIN_STRING(PRE, CENT, POST)
+
+#include JOIN_LIB_PATH(..\..\..\, HEADER_PATHS_TDNS, \MexMemoryInterfacing\Headers\MexMem.hpp)
+#include JOIN_LIB_PATH(..\..\..\, HEADER_PATHS_TDNS, \RandomNumGen\Headers\FiltRandomTBB.hpp)
 
 struct InternalVars;
 struct InputArgs;
